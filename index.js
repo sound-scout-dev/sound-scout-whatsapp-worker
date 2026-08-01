a2a0e203e63f526d4fd2bc4ac1756f9dbdbd9e5a
hiru616 <ishakyaranhiru@gmail.com>
2026-08-01 13:51:59 +0530

    Add CORS middleware headers to allow cross-origin requests from frontend

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ console.log = (...args) => {
 };
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(express.json());
 
 const WORKER_SECRET = process.env.WORKER_SECRET || 'super_secret_key';
